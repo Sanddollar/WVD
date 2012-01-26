4ef7182cde3b761a7e80e4ad2cb814d9058918ea
Sanddollar <christopher.sand@gmail.com>
2012-01-26 18:52:48 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wvd::Application.config.secret_token = '0e60ef790e404214b449b9f53af80e63659f904435495946415692b42e5c43b53e02fbc3ca21d4c08467ff6328fcc1266a882c552d06c777d94ba66fa7bb85b6'
