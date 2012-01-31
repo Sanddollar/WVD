098d94a8c453ddcb9b08864b3d4ff12fbe60f88d
Sanddollar <christopher.sand@gmail.com>
2012-01-31 00:28:28 -0500

    Began Organizing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Wvd::Application.routes.draw do
   get "pages/home"
 
+  get "pages/about"
+
   get "pages/contact"
 
   get "pages/services"
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,17 +2,45 @@ require 'spec_helper'
 
 describe PagesController do
 
+render_views
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
+ 
+  it "should have the right title" do
+    get 'home'
+    response.should have_selector("title",
+    :content => "West Village Dog Centre | Home")
+    end
   end
+
+    describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+
+   it "should have the right title" do
+    get 'about'
+    response.should have_selector("title",
+    :content => "West Village Dog Centre | About")
+    end
+  end
+
 
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
+    end
+
+   it "should have the right title" do
+    get 'contact'
+    response.should have_selector("title",
+    :content => "West Village Dog Centre | Contact")
     end
   end
 
@@ -21,6 +49,12 @@ describe PagesController do
       get 'services'
       response.should be_success
     end
+
+   it "should have the right title" do
+    get 'services'
+    response.should have_selector("title",
+    :content => "West Village Dog Centre | Service")
+    end
   end
 
   describe "GET 'amenitites'" do
@@ -28,12 +62,24 @@ describe PagesController do
       get 'amenitites'
       response.should be_success
     end
+
+   it "should have the right title" do
+    get 'amenitities'
+    response.should have_selector("title",
+    :content => "West Village Dog Centre | Amenities")
+    end
   end
 
   describe "GET 'rates'" do
     it "should be successful" do
       get 'rates'
       response.should be_success
+    end
+
+   it "should have the right title" do
+    get 'rates'
+    response.should have_selector("title",
+    :content => "West Village Dog Centre | Rates")
     end
   end
 
